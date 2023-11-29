b3ed357a863510d2d0fa4c19bc4fea2a1b513b55
Vitaliy Koltsov <vitaliy.koltsov.2002@gmail.com>
2023-11-29 15:28:59 +0300

    fix cors v3

--- ResourceServer/src/main/java/com/example/resourceserver/configs/SecurityConfig.java
+++ ResourceServer/src/main/java/com/example/resourceserver/configs/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.example.resourceserver.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -36,7 +37,7 @@ public class SecurityConfig {
 
         http
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.disable())
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(request -> request
                         .requestMatchers(toH2Console()).permitAll()
                         .requestMatchers(mvcRequestMatcher.pattern("/swagger-ui/**")).permitAll()
